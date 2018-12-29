930e3be81deb9fddd3db29e4b5229729ad5e83cf
enciie <enciie@gmail.com>
2018-12-29 00:48:52 -0800

    Set up index page

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 import './index.css';
-import App from './containers/App';
+import App from './App';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
  ReactDOM.render(
-    <Router>
-        <Provider store={store}>
+        <Provider store={ store }>
             <App />
-        </Provider>
-    </Router>,
+        </Provider>,
     document.getElementById('root')  
 );
 
