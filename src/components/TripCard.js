d748542606843ed793f1985736cc582a4f240dfa
enciie <enciie@gmail.com>
2018-12-31 18:46:19 -0800

    Add link to trip show page

--- src/components/TripCard.js
+++ src/components/TripCard.js
@@ -1,13 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import '../stylesheets/Trip.css'
 
-const Trip = ({ trip: { id, name, location, img_url, user: {username} } }) =>
+const Trip = ({ trip: { id, name, img_url, user: {username} } }) =>
     <div className="TripCard">
         <img className="TripImage" src={img_url} alt={name} />
-        <h3>{name}</h3>
-        <p>{location}</p>
+        <Link to={`trips/${id}`}>{name}</Link>
         <p>{username}</p>
     </div>
 
-export default Trip
+export default Trip;
