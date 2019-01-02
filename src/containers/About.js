f1ad89d69147219d6d238f269b4c7f8056fa9843
enciie <enciie@gmail.com>
2019-01-01 23:29:40 -0800

    Create About page container

---
+++ src/containers/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../components/NavBar';
+
+const About = () => {
+        return (
+            <div>
+                <NavBar />
+                <h1>ABOUT EPIC ADVENTURES</h1>
+                <p>A social media platform for users to share thier adventures</p>
+            </div>
+        )
+    }
+
+export default About;
