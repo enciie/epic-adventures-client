09314ed15014544ddf044e7f1e75bafdfa9c1403
enciie <enciie@gmail.com>
2019-01-02 03:31:58 -0800

    Add class name to NavBar css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,11 +6,11 @@ import '../stylesheets/NavBar.css'
 
 const NavBar = () =>
     <div className="NavBarContainer">
-         <ul>
-            <li><NavLink to='/trips'>Adventures</NavLink></li>
-            <li><NavLink to='/trips/new'>Add An Adventure</NavLink></li>
-            <li><NavLink to='/trips/mytrips' >My Adventures</NavLink></li>
-            <li><NavLink to="/about" >About</NavLink></li>
+         <ul className="NavBar">
+            <li className="NavBar"><NavLink to='/trips'>Adventures</NavLink></li>
+            <li className="NavBar"><NavLink to='/trips/new'>Add An Adventure</NavLink></li>
+            <li className="NavBar"><NavLink to='/trips/mytrips' >My Adventures</NavLink></li>
+            <li className="NavBar"><NavLink to="/about" >About</NavLink></li>
         </ul>
     </div>
 
