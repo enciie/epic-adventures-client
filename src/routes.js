94dd18757d2d9300738485be894888fd224bafa6
enciie <enciie@gmail.com>
2019-01-01 23:23:50 -0800

    Update routes and add additional routes

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,31 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 
 import Login from './containers/Login'
 import Signup from './containers/Signup'
 import Trips from './containers/Trips'
+import TripShow from './containers/TripShow'
+import About from './containers/About'
+import TripForm from './components/TripForm'
+import UserTrips from './containers/UserTrips'
 
 export default (
-    <BrowserRouter>
+    <Router>
         <Switch id='routes'>
-                <Route exact path='/' render={ () => loggedIn() ? <Trips /> : <Redirect to="/login" />} />
-                <Route path='/signup' component={ () => loggedIn() ? <Redirect to="/" /> : <Signup />} />
-                <Route path='/login' component={ () => loggedIn() ? <Redirect to="/" /> : <Login />} />
-                <Route path='/logout' component={ () => logout() } />
-                <Route path='/trips' component={ () => loggedIn() ? <Trips /> : <Redirect to="/login" />} />
+            {/* component that will be created via Route will automatically be passed the following props objects:  */}
+            {/* match, location, history */}
+            {/* macth object contains the following properties: params, isExact, path, url */}
+            <Route exact path='/' render={ () => loggedIn() ? <Trips /> : <Redirect to="/login" />} />
+            <Route path='/signup' component={ () => loggedIn() ? <Redirect to="/" /> : <Signup />} />
+            <Route path='/login' component={ () => loggedIn() ? <Redirect to="/" /> : <Login />} />
+            <Route path='/logout' component={ () => logout() } />
+            <Route exact path='/about' component={ About } />              
+            <Route exact path='/trips' component={ Trips } />
+            <Route exact path='/trips/mytrips' component={ UserTrips } />
+            <Route exact path='/trips/new' component={() => loggedIn() ? <TripForm /> : <Redirect to="/login" />} />
+            <Route exact path='/trips/:id' component={ TripShow } />
         </Switch>
-    </BrowserRouter>
+    </Router>
 )
 
 const loggedIn = () => !!sessionStorage['jwt']
