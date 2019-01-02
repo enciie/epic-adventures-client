f313c1dbb267eff20890b79fdff401069a1d197d
enciie <enciie@gmail.com>
2019-01-01 23:31:03 -0800

    Remove links and add NavBar component

--- src/containers/Trips.js
+++ src/containers/Trips.js
@@ -1,33 +1,33 @@
 import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
 
 import { fetchUser } from '../actions/userActions'
 import { fetchTrips, deleteTrip } from '../actions/tripActions'
 import { deleteComment } from '../actions/commentActions'
 
 import TripCard from '../components/TripCard'
+import NavBar from '../components/NavBar'
 
 import '../stylesheets/Trip.css'
 
 class Trips extends Component {
     componentWillMount() {
+        debugger;
         this.props.fetchUser()
         this.props.fetchTrips()
+        debugger;
     }
 
     render() {
+        console.log(this.props)
         const { user, trips } = this.props
 
         return (
             <div>
+                <NavBar />
                 <div> Logged as: {user.username} </div>
-                <nav>
-                    <Link to='/trips'>Adventures | </Link>
-                    <Link to='/trips/new'>Add An Adventure | </Link>
-                    <Link to='/trips/:trip_id'>My Adventures</Link>
-                </nav>
+            
                 <h1>EPIC ADVENTURES</h1>
                 {trips.map(trip => <TripCard key={trip.id} trip={trip} deleteTrip={this.props.deleteTrip} deleteComment={this.props.deleteComment} />)}
             </div>
@@ -36,9 +36,10 @@ class Trips extends Component {
 }
 
 const mapStateToProps = state => {
+    console.log("state", state)
     return {
         user: state.user.current,
-        trips: state.trips.all
+        trips: state.trips.trips
     }
 }
 
