e0587404f3deb1570910328a3ab559a6254d3cd3
enciie <enciie@gmail.com>
2019-01-03 02:31:44 -0800

    Update NavBar style

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,10 +7,12 @@ import '../stylesheets/NavBar.css'
 const NavBar = () =>
     <div className="NavBarContainer">
          <ul className="NavBar">
-            <li className="NavBar"><NavLink to='/trips'>Adventures</NavLink></li>
+            <li className="NavBar"><NavLink to="/logout" >Logout</NavLink></li>
+            <li className="NavBar"><NavLink to="/about" >About</NavLink></li>
             <li className="NavBar"><NavLink to='/trips/new'>Add An Adventure</NavLink></li>
             <li className="NavBar"><NavLink to='/trips/mytrips' >My Adventures</NavLink></li>
-            <li className="NavBar"><NavLink to="/about" >About</NavLink></li>
+            <li className="NavBar"><NavLink to='/trips'>Adventures</NavLink></li>
+            <li className="NavBarLogo">EPIC ADVENTURE COLLECTIVE</li>
         </ul>
     </div>
 
