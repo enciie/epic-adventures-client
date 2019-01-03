92cae1a18d6fa46a2eca4f66e43133535723d03c
enciie <enciie@gmail.com>
2019-01-03 06:21:47 -0800

    Add fetch_trip and edit_trip for trips reducer

--- src/reducers/tripsReducer.js
+++ src/reducers/tripsReducer.js
@@ -9,17 +9,23 @@ export default function tripsReducer(state = initialState, action) {
             return { ...state, trips: action.payload }
 
         case 'FETCH_TRIP':
-        debugger;
             return { ...state, trip: action.payload }
+
+        case 'FETCH_USER_TRIPS':
+        debugger;
+            return { ...state, trips: action.payload }
 
         case 'CREATE_TRIP':
             return { ...state, trips: [...state.trips, action.payload] }
+
+        case 'EDIT_TRIP':
+        debugger;
+            return { ...state.trips.filter(trip => trip.id !== action.payload.id), trips: [...state.trips, action.payload] }
 
         case 'DELETE_TRIP':
             return { ...state, trips: state.trips.filter(trip => trip.id !== action.payload.id) }
 
         case 'CREATE_COMMENT':
-        debugger;
             let trips = [...state.trips]
             let idx = trips.findIndex(trip => trip.id === action.payload.trip_id)
             trips[idx].comments.push(action.payload)
@@ -30,7 +36,7 @@ export default function tripsReducer(state = initialState, action) {
             trips = [...state.trips]
             idx = trips.findIndex(trip => trip.id === action.payload.trip_id)
             trips[idx].comments = trips[idx].comments.filter(comment => comment.id !== action.payload.id)
-
+            
             return { ...state, trips }
 
         default: return state
