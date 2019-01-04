b1514b85786821f2162f6faa3195dc1fe20ff7bb
enciie <enciie@gmail.com>
2019-01-03 23:47:14 -0800

    Add username to comments display

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Comment = ({ comment: { id, content}, user: {username}, tripId, deleteComment }) =>
   <li className="Comment">
     { content }
-    <b> - {username} - </b>
+    <b> - {username} </b>
     <button onClick={ () => deleteComment(id, tripId) }>X</button>
   </li>
 
