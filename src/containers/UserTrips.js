534c4d877da9cc30c7a47966177d48979254b06a
enciie <enciie@gmail.com>
2019-01-05 08:56:28 -0800

    Create usertrip container

---
+++ src/containers/UserTrips.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+import { fetchUser } from '../actions/userActions'
+import { fetchTrips } from '../actions/tripActions'
+
+import TripCard from '../components/TripCard'
+import NavBar from '../components/NavBar';
+
+import '../stylesheets/Trip.css'
+
+class UserTrips extends Component {
+
+    componentWillMount() {
+        this.props.fetchUser()
+        this.props.fetchTrips()
+    }
+
+    render() {
+        console.log("UserTripuser", this.props.user)
+        const { user, trips, match } = this.props
+        const userTrips = trips.filter(trip => trip.user.id === user.id)
+        console.log("UserTrips", userTrips)
+        return (
+            <div>
+                <NavBar />
+                <div className="TripListContainter">
+                    <p className="Username">Logged in as: {user.username}</p>
+                    <h1 className="Header">MY EPIC ADVENTURES</h1>
+                    {userTrips.map(trip => 
+                        <TripCard 
+                            key={trip.id} 
+                            match={match} 
+                            trip={trip} 
+                        />
+                    )}
+                </div>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    console.log("UserTripstate", state)
+    console.log("state", state)
+    return {
+        user: state.user.current,
+        trips: state.trips.all
+    }
+}
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+    fetchTrips,
+    fetchUser
+}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserTrips)
