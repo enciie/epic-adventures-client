2109c7e8ebfd7a9a0cf141923412ac19762e3bea
enciie <enciie@gmail.com>
2019-01-05 02:16:36 -0800

    Update About page and add basic styling

--- src/containers/About.js
+++ src/containers/About.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import NavBar from '../components/NavBar';
+import sunset from '../images/sunset.png';
+
+import '../stylesheets/About.css'
+
+console.log(sunset);
 
 const About = () => {
         return (
             <div>
                 <NavBar />
-                <h1>ABOUT EPIC ADVENTURES</h1>
-                <p>A social media platform for users to share thier adventures</p>
+                <div className="AboutContainer">
+                    <img className="AboutImage" src={sunset} alt="Sunset" /><br />
+                    <h1>ABOUT EPIC ADVENTURES</h1>
+                    <p>A community-driven platform for epic adventure discovery. We want to share the joy of travel and adventure, by inspiring people to discover explore, and share their own adventures.</p>
+                </div>
             </div>
         )
     }
