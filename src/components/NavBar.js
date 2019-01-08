32a156e4fd5e172492112d97a16f13280302818f
enciie <enciie@gmail.com>
2019-01-08 00:44:57 -0800

    Add logo to nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
-
+import logo from '../images/logo.png';
 import '../stylesheets/NavBar.css'
+
+console.log(logo)
 
 
 const NavBar = () =>
@@ -9,10 +11,11 @@ const NavBar = () =>
          <ul className="NavBar">
             <li className="NavBar"><NavLink to="/logout" >Logout</NavLink></li>
             <li className="NavBar"><NavLink to="/about" >About</NavLink></li>
+            <li className="NavBar"><NavLink to='/trips/inspiration'>Inspiration</NavLink></li>
             <li className="NavBar"><NavLink to='/trips/new'>Add An Adventure</NavLink></li>
             <li className="NavBar"><NavLink to='/trips/mytrips' >My Adventures</NavLink></li>
             <li className="NavBar"><NavLink to='/trips'>Adventures</NavLink></li>
-            <li className="NavBarLogo"><NavLink to='/trips'>EPIC ADVENTURE COLLECTIVE</NavLink></li>
+            <li className="NavBarLogo"><NavLink to='/trips'><img src={logo} alt="Logo" /></NavLink></li>
         </ul>
     </div>
 
